b2388ab4085a1d397cf7a7d3151622dedd4cef5c
Félix Jiménez Calvo <felixjcalvo@gmail.com>
2022-04-14 17:24:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from instagrapi import Client
+
+cl = None
+
+
+def instagram_comment_removal():
+    global cl
+    try:
+        cl = Client()
+        cl.login('ig_account', 'ig_pass')
+
+        user_id = cl.user_id_from_username("ig_account")
+        medias = cl.user_medias(int(user_id), 2)  # three last posts
+
+        posts_with_comments = [post for post in medias if post.comment_count]
+        for post in posts_with_comments:
+            post_comments = cl.media_comments(post.pk)
+            bad_words = ['crypto', 'pito']
+            bad_comments = [comment for comment in post_comments if any(word in comment.text.lower() for word in bad_words)]
+            if bad_comments:
+                cl.comment_bulk_delete(post.pk, [int(c.pk) for c in bad_comments])
+    except Exception as ex:
+        print(ex)
+    finally:
+        cl.logout()
+
+
+if __name__ == "__main__":
+    instagram_comment_removal()
